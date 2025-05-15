4bde84219f991595ca158f0e37f333584d469dd1
Utkarsh <ignition3951@gmail.com>
2025-05-15 18:40:51 +0530

    Create entities for DB

---
+++ src/main/java/com/utk/authservice/entities/UserRole.java
@@ -0,0 +1,21 @@
+package com.utk.authservice.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user_role")
+public class UserRole {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "role_id")
+    private Long roleId;
+
+    private String roleName;
+}
