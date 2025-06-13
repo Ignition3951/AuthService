0d2e6f43796b22edb2a94583c1b531fc854d9c4a
Utkarsh <ignition3951@gmail.com>
2025-06-13 13:21:53 +0530

    Add environment variable changes and introduce ping API

--- src/main/java/com/utk/authservice/controller/AuthController.java
+++ src/main/java/com/utk/authservice/controller/AuthController.java
@@ -11,6 +11,9 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,6 +48,17 @@ public class AuthController {
                     .build(), HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity(Messages.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/auth/v1/ping")
+    public ResponseEntity<String> ping(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication != null && authentication.isAuthenticated()) {
+            return ResponseEntity.ok("Pong");
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body("Unauthorized access");
         }
     }
 }
